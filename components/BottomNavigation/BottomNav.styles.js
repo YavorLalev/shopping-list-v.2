619a52a365c2a33eb71ca2bc21c7ad4ef1cf78a0
Yavor Lalev <yavorlalev@abv.bg>
2024-11-28 13:03:57 +0100

    added svg_add_icon

--- components/BottomNavigation/BottomNav.styles.js
+++ components/BottomNavigation/BottomNav.styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const NavigationBar = styled.nav`
 display: flex;
 justify-content: space-evenly;
-align-items: flex-start;
+align-items: center;
 position: fixed;
 width: 100%;
 bottom: 0;
@@ -11,4 +11,13 @@ left: 0;
 height: 60px;
 background: var(--secondary-color);
 box-shadow: 0px 1px 5px -2px rgba(0, 0, 0, 0.15);
+`
+export const AddButton = styled.button`
+appearance: none;
+border: none;
+background: inherit;
+`
+export const AddIcon = styled.img`
+width: 100%;
+height: 100%;
 `
--- components/BottomNavigation/index.js
+++ components/BottomNavigation/index.js
@@ -1,11 +1,13 @@
-import { NavigationBar } from "./BottomNav.styles";
+import { NavigationBar, AddButton, AddIcon } from "./BottomNav.styles";
 
 export default function BottomNavigation() {
 
     return (
         <>
         <NavigationBar>
-            
+            <AddButton>
+              <AddIcon src="/assets/icon_add.svg" alt="add-product"/>
+            </AddButton>
         </NavigationBar>
         </>
     )
