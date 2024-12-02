5df5ced208351cb1ed98629bd1b2207c1e0b13b0
Yavor Lalev <yavorlalev@abv.bg>
2024-12-02 12:38:33 +0100

    created form

---
+++ components/ProductForm/ProductForm.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const FormContainer= styled.form`
+display: flex;
+flex-direction: column;
+`
+export const Label=styled.label`
+margin-bottom: 10px;
+
+`
