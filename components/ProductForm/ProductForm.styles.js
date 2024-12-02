db5a628d9fb235cf61bc2ff28288d49e99459772
Yavor Lalev <yavorlalev@abv.bg>
2024-12-02 23:37:21 +0100

    adde handleSubmit function

--- components/ProductForm/ProductForm.styles.js
+++ components/ProductForm/ProductForm.styles.js
@@ -3,8 +3,14 @@ import styled from "styled-components";
 export const FormContainer= styled.form`
 display: flex;
 flex-direction: column;
+align-items: center;
+margin-top: 40px;
 `
 export const Label=styled.label`
-margin-bottom: 10px;
-
+display: flex;
+flex-direction: column;
+margin-bottom: 20px;
+`
+export const Input=styled.input`
+margin-top: 10px;
 `
--- components/ProductForm/index.js
+++ components/ProductForm/index.js
@@ -1,14 +1,29 @@
-import { FormContainer,Label } from "./ProductForm.styles";
+import { FormContainer,Label, Input} from "./ProductForm.styles";
 
 export default function ProductForm() {
 
+    function handleSubmit(event){
+        event.preventDefault()
+
+        const formData = new FormData(event.target);
+        const productData = Object.fromEntries(formData);
+
+        console.log(productData)
+    }
     return(
         <>
-        <FormContainer>
-                <Label htmlFor="name">Name:<input type="text" id="name" name="name"/></Label>
-                <Label htmlFor="quantity">Quantity:<input type="number" id="quantity" name="quantity"/></Label>
-                <Label htmlFor="category">Category:<input type="text" id="category" name="category" value=""/> </Label>
-                <button type="submit">Add Product</button>
+        <FormContainer onSubmit={handleSubmit}>
+            <Label htmlFor="name">Name: <Input type="text" id="name" name="name"/></Label>
+            
+            <Label htmlFor="quantity">Quantity:<Input type="number" id="quantity" name="quantity"/></Label>
+            
+            <Label htmlFor="category">Category: <Input type="text" id="category" name="category" /></Label>
+            
+            <button type="submit">Add Product</button>
+            
+                
+                
+               
         </FormContainer>
         </>
     );
