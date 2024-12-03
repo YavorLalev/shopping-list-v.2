ac9e4ace276de1669ce0c5531075d16e28b23740
Yavor Lalev <yavorlalev@abv.bg>
2024-12-03 10:20:01 +0100

    added measure unit select menu

--- components/ProductForm/index.js
+++ components/ProductForm/index.js
@@ -1,4 +1,5 @@
 import { FormContainer,Label, Input} from "./ProductForm.styles";
+import { measureUnits } from "@/resources/listOfMeasureUnits";
 
 export default function ProductForm() {
 
@@ -7,7 +8,6 @@ export default function ProductForm() {
 
         const formData = new FormData(event.target);
         const productData = Object.fromEntries(formData);
-
         console.log(productData)
     }
     return(
@@ -16,6 +16,14 @@ export default function ProductForm() {
             <Label htmlFor="name">Name: <Input type="text" id="name" name="name"/></Label>
             
             <Label htmlFor="quantity">Quantity:<Input type="number" id="quantity" name="quantity"/></Label>
+            <Label htmlFor="measure-unit">Choose units
+                <select id="measure-unit" name="measure-unit">
+                <option value="">Select</option>
+                {measureUnits.map((unit) => (
+                    <option key={unit} value={unit}>{unit}</option>
+                ))}
+                
+                </select></Label>
             
             <Label htmlFor="category">Category: <Input type="text" id="category" name="category" /></Label>
             
