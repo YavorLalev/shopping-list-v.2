b498084b15e76bb15714f47d644b2830ff6cd9ff
Yavor Lalev <yavorlalev@abv.bg>
2024-12-03 13:00:29 +0100

    updated global styles

--- styles.js
+++ styles.js
@@ -6,18 +6,15 @@ const workSans = Work_Sans({ subsets: ["latin"] });
 export default createGlobalStyle`
 
 :root{
-
 --primary-color: #f8f9fb;
 --secondary-color: #ff4a11;
 --input-color: #fefefe;
 --border-color: #e1ded9;
 
-
 --font-family: ${workSans.style.fontFamily};
 --font-body: 300 1rem/1.5 var(--font-family);
 --font-button: 500 1rem/1.3 var(--font-family);
 --font-label: 400 1rem/1.4 var(--font-family);
-
 }
   *,
   *::before,
