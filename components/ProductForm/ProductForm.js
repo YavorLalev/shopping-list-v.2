a3a7ab047487eb2ad274d59d8d08075e3c5d86a7
Yavor Lalev <yavorlalev@abv.bg>
2024-12-03 12:26:59 +0100

    added styled form button

--- components/ProductForm/ProductForm.js
+++ components/ProductForm/ProductForm.js
@@ -1,4 +1,4 @@
-import { FormContainer,Label, Input} from "./ProductForm.styles";
+import { FormContainer,Label, Input, Select, Fieldset, FormButton} from "./ProductForm.styles";
 import { measureUnits } from "@/resources/listOfMeasureUnits";
 
 export default function ProductForm() {
@@ -13,19 +13,28 @@ export default function ProductForm() {
     return(
         <>
         <FormContainer onSubmit={handleSubmit}>
-            <Label htmlFor="name">Name: <Input type="text" id="name" name="name"/></Label> 
-            <Label htmlFor="quantity">Quantity:<Input type="number" id="quantity" name="quantity"/></Label>
+            <Label htmlFor="name">Name: 
+                <Input type="text" id="name" name="name" required/>
+                </Label> 
+            <Fieldset>
+            <Label htmlFor="quantity">Quantity:
+                <Input type="number" id="quantity" name="quantity" required/>
+                </Label>
             <Label htmlFor="measure-unit">Choose units
-                <select id="measure-unit" name="measure-unit">
+                <Select id="measure-unit" name="measure-unit">
                 <option value="">Select</option>
                 {measureUnits.map((unit) => (
                     <option key={unit} value={unit}>{unit}</option>
                 ))}
-                </select></Label>
+                </Select>
+             </Label>
+            </Fieldset>
             
-            <Label htmlFor="category">Category: <Input type="text" id="category" name="category" /></Label>
-            <button type="submit">Add Product</button>
-               
+            
+            
+            <Label htmlFor="category">Category: <Input type="text" id="category" name="category" required /></Label>
+            <FormButton type="submit">Add Product</FormButton>
+           
         </FormContainer>
         </>
     );
--- components/ProductForm/ProductForm.styles.js
+++ components/ProductForm/ProductForm.styles.js
@@ -1,16 +1,33 @@
 import styled from "styled-components";
 
-export const FormContainer= styled.form`
+export const FormContainer = styled.form`
 display: flex;
 flex-direction: column;
 align-items: center;
 margin-top: 40px;
 `
-export const Label=styled.label`
+export const Label = styled.label`
 display: flex;
 flex-direction: column;
-margin-bottom: 20px;
+font: var(--font-label);
+margin-bottom: 24px;
 `
-export const Input=styled.input`
-margin-top: 10px;
+export const Input = styled.input`
+margin-top: 6px;
+`
+
+export const Select = styled.select`
+margin-top: 6px;
+`
+export const Fieldset = styled.fieldset`
+border: none;
+`
+export const FormButton = styled.button`
+background: var(--secondary-color);
+color: var(--primary-color);
+font: var(--font-button);
+padding: 10px;
+border-radius: 10px;
+border:none;
+cursor: pointer;
 `
