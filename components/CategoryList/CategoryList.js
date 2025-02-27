9847750a6e3f6d739eade6ea9b12209087d9ffc5
Yavor Lalev <yavorlalev@abv.bg>
2025-02-27 13:36:25 +0100

    refactor CategoryList component

--- components/CategoryList/CategoryList.js
+++ components/CategoryList/CategoryList.js
@@ -2,21 +2,26 @@ import { ListContainer, CategoryLink, CategoryItem, CategoryText } from "./Categ
 import Image from "next/image";
 
 export default function CategoryList({products}) {
-    
+
+  // const uniqueCategories = products
+  // .map(({ category}) => category) // Extract categories
+  // .filter((category, index, self) => self.indexOf(category) === index) ; // Remove duplicates
+
+  const uniqueCategories = [...new Set(products.map(({ category }) => category))];
+
+
 return (
-    <>
+    
       <ListContainer>
-        {products
-          .map(({ category}) => category) // Extract categories
-          .filter((category, index, self) => self.indexOf(category) === index) // Remove duplicates
-          .map((category, index) => (
-            <CategoryLink href = {`/products/${category}`} key={index}> 
+        {uniqueCategories
+          .map((category) => (
+            <CategoryLink href = {`/products/${category}`} key={category}> 
            <CategoryItem>
                 <Image src="/assets/Shopping cart.svg" width={140} height={140} alt="add-product" />
                 <CategoryText>{category}</CategoryText>
               </CategoryItem></CategoryLink>
           ))}
       </ListContainer>
-    </>
+    
 );
 }
