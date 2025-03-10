19f2532dc1c0a8cc43ee679fd424e7d78a9e0679
Yavor Lalev <yavorlalev@abv.bg>
2025-03-10 11:00:26 +0100

    added-logic-to-toggle-the-checkbox

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,24 @@ export default function App({ Component, pageProps }) {
 const [products, setProducts] = useLocalStorageState('products', {
   defaultValue: []});
 
-    function handleAddProduct(newProduct) {
-        setProducts([...products, {...newProduct, id: uid()}])
+  function handleAddProduct(newProduct) {
+        setProducts([...products, {...newProduct, id: uid(), isCompleted: false}])
 
 }
-console.log(products);
+
+function handleCheckboxChange(productId) {
+setProducts((prevProducts) => 
+  prevProducts.map((product) => 
+    product.id === productId ? {...product, isCompleted: !product.isCompleted } : product))
+}
+
   return (
     <>
       <GlobalStyle />
       <Component 
     {...pageProps}
     onAddProduct = {handleAddProduct}
+    onChange={handleCheckboxChange}
     products = {products}
       />
     </>
--- pages/products/[category]/index.js
+++ pages/products/[category]/index.js
@@ -10,26 +10,29 @@ height: 100vh;
 gap: 1.6rem;
 align-items: center;
 `
-export default function ProductList({products}) {
+export default function ProductList({products, onChange}) {
   const router = useRouter();
   const { category } = router.query; 
 
   const filteredProducts = products.filter(
     (product) => product.category === category
   );
-
+  
   return (
     <>
     <h2>Products in {category} list</h2>
       <ProductListContainer>
         {filteredProducts
-        .map(({id, name, quantity,measureUnit}) => (
+        .map(({id, name, quantity,measureUnit, isCompleted}) => (
           <Product
           key={id}
           id={id}
           name={name}
           quantity={quantity}
           measureUnit={measureUnit}
+          completed={isCompleted}
+          onChange={() =>onChange(id)}
+
         />
           ))}
       </ProductListContainer>
