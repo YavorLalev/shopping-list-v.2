12497f60f8005b10a67cefd1d890fc74e5e4aa7c
Yavor Lalev <yavorlalev@abv.bg>
2025-03-11 10:39:10 +0100

    fixed-DeleteButton-component

--- components/DeleteButton/DeleteButton.js
+++ components/DeleteButton/DeleteButton.js
@@ -1,11 +1,10 @@
 import { Button, DeleteIcon } from "./DeleteButton.style";
-import Image from "next/image"
 
 export default function DeleteButton ({deleteProduct}) {
 return (
 <>
 <Button  type = "button" onClick = {deleteProduct}>
-<DeleteIcon width={40} height={40} src="/assets/Bin bounce.svg" alt="remove-product"/>
+    <DeleteIcon width={32} height={32} src="/assets/Bin bounce.svg" alt="delete-product"/>
 </Button>
 </>
 );
--- components/DeleteButton/DeleteButton.style.js
+++ components/DeleteButton/DeleteButton.style.js
@@ -1,8 +1,12 @@
 import styled from "styled-components"
+
 export const Button = styled.button`
 appearance: none;
 border: none;
 background: inherit;
+position: absolute;
+top: 30px;
+right: 30px;
 `
 export const DeleteIcon = styled.img`
 cursor:pointer;
