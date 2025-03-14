4b56bc7a708f557634bfb0543608c696f3f5618c
Yavor Lalev <yavorlalev@abv.bg>
2025-03-14 11:56:42 +0100

    refactor-CategoryList

---
+++ components/CategoryDetails/CategoryDetails.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ProductListContainer = styled.ul `
+display: flex;
+flex-direction: column;
+margin-top: 40px;
+height: 100vh;
+gap: 1.2rem;
+`
+export const ProductListHeadline = styled.h3`
+font: var(--font-heading);
+color: var(--secondary-color);
+`
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-top: 80px;
+`
