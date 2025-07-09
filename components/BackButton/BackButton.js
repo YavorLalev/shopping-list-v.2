c7d136decc1b78ba3ea166c04a369e51de2a8742
Yavor Lalev <yavorlalev@abv.bg>
2025-07-09 14:01:14 +0200

    added Router

--- components/BackButton/BackButton.js
+++ components/BackButton/BackButton.js
@@ -1,8 +1,18 @@
 import { BackLink } from "./BackButton.style.js";
+import { useRouter } from "next/router";
+import Arrowleft from "../../public/lotties/ShoppingIcon.json";
 
 export default function BackButton() {
+const router = useRouter();
 
+  const handleClick = (event) => {
+    event.preventDefault();
+    router.back();
+  };
     return (<>
-    <p> Hello </p>
+    <BackLink href="#" onClick={handleClick}>
+
+Back 
+    </BackLink>
     </>)
 }
